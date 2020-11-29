e87e864dd51b64d893fd5fa5ff394785addab081
Andrey Shalashov <avshalashov@yandex.ru>
2020-11-29 22:03:06 +0300

    present http echo

--- cmd/goparrot.go
+++ cmd/goparrot.go
@@ -1,9 +1,46 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 	"github.com/k1nky/goparrot/internal/config"
 )
 
+var (
+	Config *config.Config
+)
+
+type EchoHTTPHandler func(w http.ResponseWriter, r *http.Request)
+
+func makeHTTPHandler(options config.HandlerConfig) EchoHTTPHandler {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(options.Code)
+	}
+}
+
 func main() {
-	config.LoadConfig("../internal/config/tests/valid.yaml")
+	var err error
+
+	if Config, err = config.LoadConfig("../internal/config/tests/valid.yaml"); err != nil {
+		log.Fatalln("Config is invalid")
+	}
+
+	router := mux.NewRouter()
+	for _, h := range Config.Handlers {
+		if h.Type == "http" {
+			router.HandleFunc(h.Prefix, makeHTTPHandler(h))
+		}
+	}
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         fmt.Sprintf("%s:%s", Config.Listen, Config.Port),
+		WriteTimeout: 10 * time.Second,
+		ReadTimeout:  10 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/go-delve/delve v1.5.0
+	github.com/gorilla/mux v1.8.0
 	gopkg.in/yaml.v2 v2.4.0
 )

1ac2dda210f4696da7148c5e8de6b3f9e2461677
Andrey Shalashov <avshalashov@yandex.ru>
2020-11-30 21:59:03 +0300

    add lua handler

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"context"
+	"io"
 	"net/http"
+	"time"
 
 	"github.com/k1nky/goparrot/internal/config"
+	lua "github.com/yuin/gopher-lua"
 )
 
 type EchoHTTPHandler func(w http.ResponseWriter, r *http.Request)
 
+func runLuaBlock(code string) string {
+	lvm := lua.NewState(lua.Options{
+		CallStackSize:       120,
+		MinimizeStackMemory: true,
+	})
+	defer lvm.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	lvm.SetContext(ctx)
+
+	if err := lvm.DoString(code); err != nil {
+		return ""
+	}
+	lv := lvm.Get(-1)
+	if str, ok := lv.(lua.LString); ok {
+		return string(str)
+	}
+	return ""
+}
+
 func makeHTTPHandler(options config.HandlerConfig) EchoHTTPHandler {
 	return func(w http.ResponseWriter, r *http.Request) {
+		if options.ResponseLua != "" {
+			body := runLuaBlock(options.ResponseLua)
+			io.WriteString(w, body)
+
+		}
 		w.WriteHeader(options.Code)
 	}
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/go-delve/delve v1.5.0
 	github.com/gorilla/mux v1.8.0
+	github.com/yuin/gopher-lua v0.0.0-20200816102855-ee81675732da
 	gopkg.in/yaml.v2 v2.4.0
 )
